730ae62d2ecc955590e8fc1536791b8648e9e344
Cristian-Stefan Tutuianu <stutuian@amazon.co.uk>
2018-08-30 20:58:58 +0100

    Added first unit test

--- src/pu-clients/pu-okcupid/__tests__/Utils.test.ts
+++ src/pu-clients/pu-okcupid/__tests__/Utils.test.ts
@@ -1,7 +1,31 @@
-// import {getOkCupidHeaders} from "../Utils";
+import {getOkCupidHeaders} from "../Utils";
 
 describe("Utils", ()=> {
-    test("getOkCupidHeaders", () => {
-        expect(true);
+    
+    describe("getOkCupidHeaders", ()=> {
+        
+        test("# should get headers with oauthToken if argument is present", () => {
+            const mockOauthToken = "mockOauthToken";
+            const expectedOutput = {
+                'x-okcupid-platform':'DESKTOP',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+                'authorization' : 'Bearer mockOauthToken'
+              }
+           
+            const actual =  getOkCupidHeaders(mockOauthToken);
+    
+            expect(actual).toEqual(expectedOutput);
+        });
+
+        test("# should get headers without oauthToken if argument is not present", () => {
+            const expectedOutput = {
+                'x-okcupid-platform':'DESKTOP',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
+              }
+           
+            const actual = getOkCupidHeaders();
+    
+            expect(actual).toEqual(expectedOutput);
+        });
     });
 })
